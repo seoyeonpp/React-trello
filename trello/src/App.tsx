60e9b652fb49f73a8c3e3dc3b2b93e8d869bf349
박서연 <seoyeonpub@gmail.com>
2023-01-05 19:31:47 +0900

    recoil selector 학습

---
+++ trello/src/App.tsx
@@ -0,0 +1,18 @@
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { hourSelector, minutesState } from './atoms';
+
+function App() {
+  const [minutes, setMinutes] = useRecoilState(minutesState)
+  const hours = useRecoilValue(hourSelector);
+  const onMinuteChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setMinutes(+event.currentTarget.value)
+  }
+  return (
+    <div>
+      <input value={minutes} onChange={onMinuteChange} type="number" placeholder='Minutes' />
+      <input value={hours} type="number" placeholder='Hours' />
+    </div>
+  );
+}
+
+export default App;
---
+++ trello/src/theme.ts
@@ -0,0 +1,7 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+    bgColor: "#2C3A47",
+    textColor: "#fff",
+    accentColor: "#FD7272",
+}
